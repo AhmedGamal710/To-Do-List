10e703cfb3185f117f46d8274ff6755bf982c47e
Ahmed Gamal <63549938+AhmedGamal710@users.noreply.github.com>
2021-09-18 18:40:08 +0100

    create to do list

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { ToolTipDirective } from './shared/tool-tip.directive';
+import { ToolTipSingletonDirective } from './shared/tool-tip-singleton.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    EditTodoDialogComponent,
+    ToolTipDirective,
+    ToolTipSingletonDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
